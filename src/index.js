a25828e9ca21ff43001a62d3cf9fc1e887f680d8
zuoguangcheng <zuoguangcheng@shuidao.com>
2018-07-23 11:12:05 +0800

    添加template webapck  优化index.js文件

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const optimist = require('optimist')
+const chalk = require('chalk');
 
 var argv = optimist.argv
 const commands = argv._;
+var params = ['init', 'build', 'start'];
 
-let command = '';
-try {
-  command = require(`./command/${commands[0]}`);
-} catch (e) {
-  console.log('输入错误', e);
+let enter = () => {
+  if (commands.length <= 0) {
+    console.log(chalk.red('请输入参数: init; build; start'));
+    return false;
+  } else if (!params.includes(commands[0])) {
+    console.log(chalk.red('输入参数错误-请输入参数: init; build; start'));
+    return false;
+  }
+
+  let command = '';
+  try {
+    command = require(`./command/${commands[0]}`);
+  } catch (e) {
+    console.log('输入错误', e);
+  }
+
+  argv.cwd = process.cwd();
+  command(argv);
 }
 
-argv.cwd = process.cwd();
-command(argv);
+enter();
